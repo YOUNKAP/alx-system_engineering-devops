2dac458f14b67f6242e0888658b495e5c2c21ac4
YOUNKAP <nina.younkap@ccaa.pro>
2024-01-17 14:27:32 -0500

    update advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the number of
-subscribers (not active users, total subscribers) for a given subreddit"""
 
-import requests
+import requests as r
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers from the Reddit API"""
-    r = requests.get(r'https://www.reddit.com/r/{}/about.json'
-                     .format(subreddit), headers={'User-agent': 'x'},
-                     allow_redirects=False)
-    if r.status_code != 200:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
+    }
+    response = r.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    json = r.json()
-    data = json.get('data')
-    return data.get('subscribers', 0)
+    results = response.json().get("data")
+    return results.get("subscribers")
